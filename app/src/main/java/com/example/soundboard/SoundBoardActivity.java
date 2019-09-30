94518ffc89083891c984dacf66693caf4d02a541
KaydenDaKat <gippy713@gmail.com>
2019-09-30 13:22:29 -0700

    Scale button is now functional

--- app/src/main/java/com/example/soundboard/SoundBoardActivity.java
+++ app/src/main/java/com/example/soundboard/SoundBoardActivity.java
@@ -16,13 +16,13 @@ public class SoundBoardActivity extends AppCompatActivity implements View.OnClic
 
 
     private Button ButtonA, ButtonB, ButtonBfl, ButtonC, ButtonCsh, ButtonD, ButtonDsh, ButtonE, ButtonF, ButtonFSh, ButtonG, ButtonGsh, ButtonchangeOctave;
-    private Button buttonScale1, buttonScale2;
+    private Button buttonScale1, Song1;
     public boolean octave;
     private SoundPool soundPool;
     private int soundID;
     boolean loaded = false;
-    private int[] sounds1,sounds2;
-    private Map<Integer, Integer> noteMap,noteMap2;
+    private int[] sounds1, sounds2;
+    private Map<Integer, Integer> noteMap, noteMap2;
 
 
     @Override
@@ -38,9 +38,9 @@ public class SoundBoardActivity extends AppCompatActivity implements View.OnClic
         // Set the hardware buttons to control the music
 
 
-        }
-    public void wireWidgets()
-    {
+    }
+
+    public void wireWidgets() {
         ButtonA = findViewById(R.id.button_main_a);
         ButtonB = findViewById(R.id.button_main_B);
         ButtonBfl = findViewById(R.id.button_main_Bfl);
@@ -55,81 +55,85 @@ public class SoundBoardActivity extends AppCompatActivity implements View.OnClic
         ButtonGsh = findViewById(R.id.button_main_Gsh);
         ButtonchangeOctave = findViewById(R.id.button_main_changeOctave);
         buttonScale1 = findViewById(R.id.button_main_scale1);
-        buttonScale2 = findViewById(R.id.button_main_scale2);
+        Song1 = findViewById(R.id.button_main_scale2);
 
 
     }
 
-public void setListeners()
-{
-    ButtonchangeOctave.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            octave = !octave;
+    public void setListeners() {
+        ButtonchangeOctave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                octave = !octave;
 
+            }
+        });
+
+        buttonScale1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                playScale1();
+            }
+        });
+
+        KeyBoardListener keyBoardLister = new KeyBoardListener();
+        ButtonA.setOnClickListener(keyBoardLister);
+        ButtonB.setOnClickListener(keyBoardLister);
+        ButtonBfl.setOnClickListener(keyBoardLister);
+        ButtonC.setOnClickListener(keyBoardLister);
+        ButtonCsh.setOnClickListener(keyBoardLister);
+        ButtonD.setOnClickListener(keyBoardLister);
+        ButtonDsh.setOnClickListener(keyBoardLister);
+        ButtonE.setOnClickListener(keyBoardLister);
+        ButtonF.setOnClickListener(keyBoardLister);
+        ButtonFSh.setOnClickListener(keyBoardLister);
+        ButtonG.setOnClickListener(keyBoardLister);
+        ButtonGsh.setOnClickListener(keyBoardLister);
+
+    }
+
+    private void delay(int millisDelay) {
+        try {
+            Thread.sleep(millisDelay);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-    });
-
-    KeyBoardListener keyBoardLister = new KeyBoardListener();
-    ButtonA.setOnClickListener(keyBoardLister);
-    ButtonB.setOnClickListener(keyBoardLister);
-    ButtonBfl.setOnClickListener(keyBoardLister);
-    ButtonC.setOnClickListener(keyBoardLister);
-    ButtonCsh.setOnClickListener(keyBoardLister);
-    ButtonD.setOnClickListener(keyBoardLister);
-    ButtonDsh.setOnClickListener(keyBoardLister);
-    ButtonE.setOnClickListener(keyBoardLister);
-    ButtonF.setOnClickListener(keyBoardLister);
-    ButtonFSh.setOnClickListener(keyBoardLister);
-    ButtonG.setOnClickListener(keyBoardLister);
-    ButtonGsh.setOnClickListener(keyBoardLister);
-    buttonScale1.setOnClickListener(keyBoardLister);
-    buttonScale2.setOnClickListener(keyBoardLister);
-}
-
-private void delay(int millisDelay)
-{
-    try {
-        Thread.sleep(millisDelay);
-    } catch (InterruptedException e) {
-        e.printStackTrace();
     }
-}
 
     public void createSounds() {
 
 
         noteMap = new HashMap<>();
-        noteMap.put(ButtonA.getId(),sounds1[0]);
-        noteMap.put(ButtonB.getId(),sounds1[1]);
-        noteMap.put(ButtonBfl.getId(),sounds1[2]);
-        noteMap.put(ButtonC.getId(),sounds1[3]);
-        noteMap.put(ButtonCsh.getId(),sounds1[4]);
-        noteMap.put(ButtonD.getId(),sounds1[5]);
-        noteMap.put(ButtonDsh.getId(),sounds1[6]);
-        noteMap.put(ButtonE.getId(),sounds1[7]);
-        noteMap.put(ButtonF.getId(),sounds1[8]);
-        noteMap.put(ButtonFSh.getId(),sounds1[9]);
-        noteMap.put(ButtonG.getId(),sounds1[10]);
-        noteMap.put(ButtonGsh.getId(),sounds1[11]);
+        noteMap.put(ButtonA.getId(), sounds1[0]);
+        noteMap.put(ButtonB.getId(), sounds1[1]);
+        noteMap.put(ButtonBfl.getId(), sounds1[2]);
+        noteMap.put(ButtonC.getId(), sounds1[3]);
+        noteMap.put(ButtonCsh.getId(), sounds1[4]);
+        noteMap.put(ButtonD.getId(), sounds1[5]);
+        noteMap.put(ButtonDsh.getId(), sounds1[6]);
+        noteMap.put(ButtonE.getId(), sounds1[7]);
+        noteMap.put(ButtonF.getId(), sounds1[8]);
+        noteMap.put(ButtonFSh.getId(), sounds1[9]);
+        noteMap.put(ButtonG.getId(), sounds1[10]);
+        noteMap.put(ButtonGsh.getId(), sounds1[11]);
         noteMap2 = new HashMap<>();
-        noteMap2.put(ButtonA.getId(),sounds2[0]);
-        noteMap2.put(ButtonB.getId(),sounds2[1]);
-        noteMap2.put(ButtonBfl.getId(),sounds2[2]);
-        noteMap2.put(ButtonC.getId(),sounds2[3]);
-        noteMap2.put(ButtonCsh.getId(),sounds2[4]);
-        noteMap2.put(ButtonD.getId(),sounds2[5]);
-        noteMap2.put(ButtonDsh.getId(),sounds2[6]);
-        noteMap2.put(ButtonE.getId(),sounds2[7]);
-        noteMap2.put(ButtonF.getId(),sounds2[8]);
-        noteMap2.put(ButtonFSh.getId(),sounds2[9]);
-        noteMap2.put(ButtonG.getId(),sounds2[10]);
-        noteMap2.put(ButtonGsh.getId(),sounds2[11]);
+        noteMap2.put(ButtonA.getId(), sounds2[0]);
+        noteMap2.put(ButtonB.getId(), sounds2[1]);
+        noteMap2.put(ButtonBfl.getId(), sounds2[2]);
+        noteMap2.put(ButtonC.getId(), sounds2[3]);
+        noteMap2.put(ButtonCsh.getId(), sounds2[4]);
+        noteMap2.put(ButtonD.getId(), sounds2[5]);
+        noteMap2.put(ButtonDsh.getId(), sounds2[6]);
+        noteMap2.put(ButtonE.getId(), sounds2[7]);
+        noteMap2.put(ButtonF.getId(), sounds2[8]);
+        noteMap2.put(ButtonFSh.getId(), sounds2[9]);
+        noteMap2.put(ButtonG.getId(), sounds2[10]);
+        noteMap2.put(ButtonGsh.getId(), sounds2[11]);
 
     }
 
-    public void createSoundList()
-    {
+    public void createSoundList() {
         this.setVolumeControlStream(AudioManager.STREAM_MUSIC);
         // Load the sound
         soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
@@ -167,23 +171,52 @@ private void delay(int millisDelay)
         sounds2[10] = soundPool.load(this, R.raw.scalehighg, 1);
         sounds2[11] = soundPool.load(this, R.raw.scalehighgs, 1);
     }
+
+    public void playNote(int arrayNum,int note)
+    {
+        if (arrayNum == 1) {
+            soundPool.play(sounds1[note], 1, 1, 1, 0, 1f);
+        }
+        else
+        {
+            soundPool.play(sounds2[note], 1, 1, 1, 0, 1f);
+        }
+    }
+    public void playScale1()
+    {
+        playNote(1,3);
+        delay(500);
+        playNote(1,5);
+        delay(500);
+        playNote(1,7);
+        delay(500);
+        playNote(1,8);
+        delay(500);
+        playNote(1,10);
+        delay(500);
+        playNote(2,0);
+        delay(500);
+        playNote(2,1);
+        delay(500);
+        playNote(2,3);
+    }
+
     @Override
     public void onClick(View view) {
     }
-    private class KeyBoardListener implements View.OnClickListener
-    {
+
+    private class KeyBoardListener implements View.OnClickListener {
 
         @Override
         public void onClick(View view) {
 
 
-            if(!octave) {
+            if (!octave) {
                 int songId = noteMap.get(view.getId());
                 if (songId != 0) {
                     soundPool.play(songId, 1, 1, 1, 0, 1f);
                 }
-            }
-            else {
+            } else {
                 int songId2 = noteMap2.get(view.getId());
                 if (songId2 != 0) {
                     soundPool.play(songId2, 1, 1, 1, 0, 1f);
